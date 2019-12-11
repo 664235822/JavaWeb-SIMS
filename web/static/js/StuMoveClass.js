a8243092e46841cdce7c5843e6f29c46e20eeb48
wangbaba <2581009220@qq.com>
2019-12-11 09:24:24 +0800

    添加js文件

--- web/static/js/StuMoveClass.js
+++ web/static/js/StuMoveClass.js
@@ -7,14 +7,16 @@ layui.use('table', function(){
         elem: '#LAY_table_user',
         url: '/demo/table/user/',
         cols: [[
+            //以下命名不准确，以实际数据库为准
             {checkbox: true, fixed: true},
-            {field:'id', title: '学生编号', width:180, sort: true, fixed: true},
-            {field:'username', title: '学生姓名', width:160},
-            {field:'year', title: '学生年龄', width:160},
-            {field:'sex', title: '性别', width:120, sort: true} ,
-            {field:'grade', title: '当前年级', width:180},
-            {field:'class', title: '当前班级', width:180},
-            {fixed: 'right', title:'操作', toolbar: '#barDemo', width:160}
+            {field:'id', title: '学生编号',  sort: true, fixed: true},
+            {field:'username', title: '学生姓名', align: 'center'},
+            {field:'year', title: '学生年龄',align: 'center'},
+            {field:'sex', title: '性别',   sort: true} ,
+            {field:'grade', title: '当前年级'},
+            {field:'class', title: '当前班级'},
+            {field:'teacher', title: '班级教师',align: 'center'},
+            {fixed: 'right', title:'操作', toolbar: '#barDemo', align: 'center'}
         ]],
         id: 'testReload',
         page: true,

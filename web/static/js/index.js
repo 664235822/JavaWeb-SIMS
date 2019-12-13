a42596a95265484e001fc918350b1ed7f83e308a
SmallPig1997 <664235822@qq.com>
2019-12-13 11:37:00 +0800

    修改主页样式

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -31,7 +31,7 @@ $(function () {
     var url = "/JavaWeb_SIMS_war_exploded/menu";
     var menu = Ajax(url, str);
     Menu(menu);
-    $(".nav a.userlist").click(function () {
+    $("a.userlist").click(function () {
         var url="/JavaWeb_SIMS_war_exploded/static/html/"+$(this).attr("name");
         if($(this).attr("name")!=undefined){
             $(".layui-body>iframe").attr("src",url);
@@ -46,27 +46,23 @@ $(function () {
 function Menu(Menu) {
     var menuName=Menu.data;
     var text = "";
-    text += "<ul class=\"firstnv nav nav-pills nav-stacked\" id=\"menu\">";
+    text += "<ul class=\"layui-nav layui-nav-tree\" id=\"menu\">";
     for(var i=0;i<menuName.length;i++){
-        text += "<li role=\"presentation\" id=\"Menu"+i+"\"><a  href=\"#\">";
-        text += "<span class=\"glyphicon glyphicon-list-alt Font-list\" aria-hidden=\"true\"></span>";
+        text += "<li class=\"layui-nav-item\" id=\"Menu"+i+"\"><a  href=\"#\">";
         text += menuName[i].menuName;
-        text += "<div class=\"pull-right\"><span class=\"caret\"></span></div></a></li>";
-        text += "<li role=\"presentation\" id=\"Menu"+i+"_Submenu\" style=\"display: none;\"><div class=\"row\"><div class=\"col\">";
-        text += "<ul class=\"nav nav-pills nav-stacked\">";
+        text += "</a>";
+        text += "<dl class=\"layui-nav-child\" id=\"Menu"+i+"_Submenu\">";
         for (var j=0;j<menuName[i].items.length;j++){
-            text += "<li role=\"presentation\">";
-            text += "<a class=\"userlist\" name=\""+ menuName[i].items[j].url+"\" href=\"#\">";
-            text += "<span class=\"glyphicon glyphicon-triangle-right Font-list\" aria-hidden=\"true\"></span>";
+            text += "<dd><a class='userlist' name=\""+ menuName[i].items[j].url+"\">";
             text += menuName[i].items[j].menuName;
             text += "</a>";
-            text += "</li>";
+            text += "</dd>";
         }
-        text += "</ul>";
-        text += "</div></div></li>";
+        text += "</dl>";
+        text += "</li>";
     }
     text += "</ul>";
-    $(".menue-box .col").html(text);
+    $(".layui-side-scroll").html(text);
 }
 $(function () {
     var Contral=null;

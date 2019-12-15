66ba917b54d9bb9e609dc91ab23adcfa6b371854
QTMY <yq1271309617@163.com>
2019-12-15 20:19:08 +0800

    权限区别，控制开关

--- web/static/js/StateManager.js
+++ web/static/js/StateManager.js
@@ -6,19 +6,68 @@
 //权限管理
 $(function () {
     Refresh();
+    var TeacherMenu=getPage({"character":"TeacherMenu","currentPage":0,"getId":'true'});
     var str = {"character":"AdminMenu","currentPage":1,"getId":'false'};
-    var url = "/JavaWeb_SIMS_war_exploded/menu";
-    var menu = Ajax(url, str);
+    var menu = getPage(str);
     if (menu.code == 1) {
         StateTable(menu.data);
         Refresh();
         Page("test1",menu.data.pageCount,menu.data.dataCount);
-        // TeacherFunction();
+        TeacherState(TeacherMenu)
     }
 
 })
+//教师权限
+function TeacherState(TeacherMenu) {
+    if(TeacherMenu.code==1){
+        var menuId=TeacherMenu.data;
+        $("#table tbody tr").each(function() {
+            for(var i=0;i<menuId.length;i++){
+                if($(this).attr("name")==menuId[i]){
+                    $("#table tr[name="+$(this).attr("name")+"]").find("input[type=checkbox]").attr("checked", true);
+                    break;
+                }
+            }
+        });
+    }
+}
+//学生权限
+function StudentState(StudentsMenu) {
+    if(StudentsMenu.code==1){
+        var menuId=StudentsMenu.data;
+        $("#table tbody tr").each(function() {
+            for(var i=0;i<menuId.length;i++){
+                if($(this).attr("name")==menuId[i]){
+                    $("#table tr[name="+$(this).attr("name")+"]").find("input[type=checkbox]").attr("checked", true);
+                    break;
+                }
+            }
+        });
+    }
+}
+
+//权限内容区别
+function StateIf(index) {
+    $("#table tbody tr").each(function() {
+        $("#table tr[name="+$(this).attr("name")+"]").find("input[type=checkbox]").attr("checked", false);
+    });
+    if(index==0){
+        var TeacherMenu=getPage({"character":"TeacherMenu","currentPage":0,"getId":'true'});
+        TeacherState(TeacherMenu);
+    }
+    if (index==1){
+        var StudentsMenu=getPage({"character":"StudentMenu","currentPage":0,"getId":'true'})
+        StudentState(StudentsMenu);
+    }
+}
+function getPage(data) {
+    var url = "/JavaWeb_SIMS_war_exploded/menu";
+    var table = Ajax(url, data);
+    return table;
+}
 //分页
 function Page(id,limit,count) {
+    var index=0;
     layui.use('laypage', function () {
         var laypage = layui.laypage;
 
@@ -36,11 +85,21 @@ function Page(id,limit,count) {
                     var table = Ajax(url, data);
                     if (table.code == 1) {
                         StateTable(table.data,obj.curr);
+                        StateIf(index);
                         Refresh();
-
                     }
                 }
             }
+        });
+
+    });
+    layui.use('element', function(){
+        var element = layui.element;
+        element.render();
+        element.on('tab(docDemoTabBrief)', function(data){
+            index=data.index;
+            StateIf(index);
+            Refresh();
         });
     });
 }
@@ -56,7 +115,7 @@ function StateTable(menu) {
         text += "</thead>";
         text += "<tbody>";
         for(var i=0;i<MenuTable.length;i++){
-            text += "<tr>";
+            text += "<tr name=\""+MenuTable[i].menuId+"\">";
             text += "<td>"+MenuTable[i].menuId+"</td>";
             if(MenuTable[i].parent==0){
                 var id=MenuTable[i].menuId
@@ -66,7 +125,7 @@ function StateTable(menu) {
             }else {
                 for(var key in tableMenu){
                    if(key==MenuTable[i].parent){
-                       text += "<td>"+tableMenu[key]+"</td>";
+                       text += "<td name="+key+">"+tableMenu[key]+"</td>";
                        text += "<td>"+MenuTable[i].menuName+"</td>";
                    }
                 }

3325a072177ee7db8ce7bacf8889c603d35c82d0
QTMY <yq1271309617@163.com>
2019-12-24 09:29:18 +0800

    添加首页页面

--- web/static/js/Login.js
+++ web/static/js/Login.js
@@ -60,7 +60,7 @@ $(function () {
                     , time: 1000
                 }, function () {
                     $("#LAY-user-login-vercode").val("");
-                    location.href = "/JavaWeb_SIMS_war_exploded/"
+                    location.href = "/JavaWeb_SIMS_war_exploded/static/html/HomePage.html"
                 });
             }else{
                 layer.msg(Menu.message, {

22ed838d4a08a6a7ed817cc31ecbdda80d503fc9
wangbaba <2581009220@qq.com>
2019-12-26 19:58:14 +0800

    科目管理js部分

--- web/static/js/SubjectManager.js
+++ web/static/js/SubjectManager.js
@@ -292,14 +292,7 @@ function gradesmana(codeList) {
 
 }
 
-//获取页面
-function getPage(data) {
-    var url = "/JavaWeb_SIMS_war_exploded/select";
-    var table = Ajax(url, data);
-    return table;
-}
-
-//分页
+//科目管理分页
 function Page(id, limit, count) {
     layui.use('laypage', function () {
         var laypage = layui.laypage;
@@ -364,17 +357,12 @@ function Subteacher() {
 //表格主体
 function tabsubt(data) {
     var text = "";
-    text += " <colgroup> <col width=\"50\"><col width=\"150\"><col width=\"200\"><col></colgroup>";
     text += "<thead><tr>";
-    text += "<th><div class=\"layui-form\" id=\"allChoose\"> <input  type=\"checkbox\" name=\"delete\" title=\"\" lay-skin=\"primary\" >";
-    text += "</div></th>"
     text += "<th>科目编号</th><th>科目名称</th><th>所在年级</th><th>所在班级</th><th>班级当前教师</th><th>操作</th>";
     text += "</tr></thead>";
     text += "<tbody>";
     for (var i = 0; i < data.length; i++) {
         text += "<tr name=\'" + data[i].subjectCode + "\'>";
-        text += "<td><div class=\"layui-form\"> <input type=\"checkbox\" name=\"checkbox\" title=\"\" lay-skin=\"primary\" >";
-        text += "</div></td>"
         text += "<td>" + data[i].subjectCode + "</td>";
         text += "<td>" + data[i].subjectName + "</td>";
         text += "<td>" + data[i].gradeName + "</td>";
@@ -387,6 +375,92 @@ function tabsubt(data) {
         text += "</tbody>";
         $("#subteatable").html(text);
     }
+}
+
+
+/**         查看科任老师信息**/
+//预加载
+function showteachifo() {
+    var url = "/JavaWeb_SIMS_war_exploded/select";
+    var data = {"tableName": "SubjectTeacher", "gradeId": 0, "classId": 0,"subjectId":0, "currentPage": 1};
+    var dataa = Ajax(url, data);
+    if (dataa.code == 1) {
+        tabteainfo(dataa.data.list);
+        Page2("test1", dataa.data.pageCount, dataa.data.dataCount);
+        Refresh();
+    }
+}
+//表格主体
+function tabteainfo(data) {
+    var text = "";
+    text += "<thead><tr>";
+    text += "<th>教师编号</th><th>教师姓名</th><th>教师性别</th><th>所教科目</th><th>教师学历</th><th>教师年龄</th>";
+    text += "</tr></thead>";
+    text += "<tbody>";
+    for (var i = 0; i < data.length; i++) {
+        text += "<tr name=\'" + data[i].teacherId + "\'>";
+        text += "<td>" + data[i].code + "</td>";
+        text += "<td>" + data[i].name + "</td>";
+        text += "<td>" + data[i].sex + "</td>";
+        text += "<td>" + data[i].subjectName+ "</td>";
+        text += "<td>" + data[i].education + "</td>";
+        text += "<td>" + data[i]. age + "</td>";
+        text += "</tr>";
+        text += "</tbody>";
+        $("#subteatable").html(text);
+    }
+}
+
+//查看科任教师分页
+function Page2(id, limit, count) {
+    layui.use('laypage', function () {
+        var laypage = layui.laypage;
+        //执行一个laypage实例
+        laypage.render({
+            elem: 'test1' //注意，这里的 test1 是 ID，不用加 # 号
+            , count: count //数据总数，从服务端得到
+            , limit: 10
+            , layout: ['prev', 'page', 'next', 'skip']
+            , jump: function (obj, first) {
+                //首次不执行
+                if (!first) {
+                    var name = $("#sous").val();
+                    var data = {"tableName": "SubjectTeacher", "gradeId": 0, "classId": 0,"subjectId":0, "currentPage": obj.curr};
+                    var table = getPage(data);
+                    if (table.code == 1) {
+                        tabteainfo(dataa.data.list);
+                        Page2("test1", dataa.data.pageCount, dataa.data.dataCount);
+                        Refresh();
+                    }
+                }
+            }
+        });
+    });
+}
+//内联搜索
+function checktab() {
+
+    $(function () {
+        //查询
+        $("#subsea1").click(function () {
+            var name = $("#LAY_sub").val();
+            var data = {"tableName": "Subject", "code": "", "name": name, "currentPage": 1};
+            var table = getPage(data);
+            if (table.code == 1) {
+                Tabsub(table.data.list);
+                Refresh();
+                Page("test1", table.data.pageCount, table.data.dataCount);
+                generalmang();
+            }
+        });
+    });
+}
+
+//获取页面
+function getPage(data) {
+    var url = "/JavaWeb_SIMS_war_exploded/select";
+    var table = Ajax(url, data);
+    return table;
 }
 
 //刷新
